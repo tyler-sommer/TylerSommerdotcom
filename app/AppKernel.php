a16541bc7127fae8549725f49db1595e6671f73d
Tyler Sommer <sommertm@gmail.com>
2012-10-10 21:02:53 -0700

    Generated 'BlogBundle'

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new TylerSommer\Bundle\BlogBundle\TylerSommerBlogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/TylerSommer/Bundle/BlogBundle/TylerSommerBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TylerSommer\Bundle\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TylerSommerBlogBundle extends Bundle
+{
+}
