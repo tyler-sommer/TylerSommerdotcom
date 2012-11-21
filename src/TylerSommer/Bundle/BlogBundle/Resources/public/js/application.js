0e36693ac34009cfef2c1a2e779bc5a32cb19fa1
Tyler Sommer <sommertm@gmail.com>
2012-11-21 15:57:49 -0800

    Added javascript select2 bindings on Page crud, and fixed some small display issues with select2

--- src/TylerSommer/Bundle/BlogBundle/Resources/public/js/application.js
+++ src/TylerSommer/Bundle/BlogBundle/Resources/public/js/application.js
@@ -3,6 +3,6 @@
     var values = $(this).data('values'),
       selected = $(this).val();
 
-    $(this).val(selected).select2({ tags: values.split(','), width: '200px' });
+    $(this).val(selected).select2({ tags: values.split(','), width: '220px' });
   }
 })(jQuery);
