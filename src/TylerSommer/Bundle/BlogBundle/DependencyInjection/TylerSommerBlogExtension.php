d0b6b4eeb0ccf86087f9f5f5b79219d61d947f27
Tyler Sommer <sommertm@gmail.com>
2014-10-06 14:35:11 -0700

    Added app version

--- src/TylerSommer/Bundle/BlogBundle/DependencyInjection/TylerSommerBlogExtension.php
+++ src/TylerSommer/Bundle/BlogBundle/DependencyInjection/TylerSommerBlogExtension.php
@@ -4,15 +4,11 @@ namespace TylerSommer\Bundle\BlogBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
-class TylerSommerBlogExtension extends Extension
+class TylerSommerBlogExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -24,5 +20,15 @@ class TylerSommerBlogExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->setParameter('app.version', \AppKernel::VERSION);
     }
 }
