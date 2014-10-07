34c500256b8074cff22ac2238865d28d90617739
Tyler Sommer <sommertm@gmail.com>
2014-10-07 15:41:45 -0700

    Added correct license

--- src/TylerSommer/Bundle/BlogBundle/Entity/Category.php
+++ src/TylerSommer/Bundle/BlogBundle/Entity/Category.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Copyright (c) Tyler Sommer
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace TylerSommer\Bundle\BlogBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
