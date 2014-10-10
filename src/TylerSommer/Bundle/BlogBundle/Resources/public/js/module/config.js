324a06f70736c651cd79ea4dc16dd70cdd59a146
Tyler Sommer <sommertm@gmail.com>
2014-10-09 20:18:29 -0700

    Fixed entity handling in ckeditor config

--- src/TylerSommer/Bundle/BlogBundle/Resources/public/js/module/config.js
+++ src/TylerSommer/Bundle/BlogBundle/Resources/public/js/module/config.js
@@ -1,9 +1,9 @@
 CKEDITOR.editorConfig = function(config) {
   config.forcePasteAsPlainText = false;
-  config.basicEntities = false;
-  config.entities = false;
-  config.entities_latin = false;
-  config.entities_greek = false;
+  config.basicEntities = true;
+  config.entities = true;
+  config.entities_latin = true;
+  config.entities_greek = true;
   config.entities_processNumerical = false;
   config.fillEmptyBlocks = function (element) {
     return true;
