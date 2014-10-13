5d1f8c8e6251a5d6c9b1fbae8734149f39f482f6
Tyler Sommer <sommertm@gmail.com>
2014-10-13 09:29:12 -0700

    Prepped release

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,7 +5,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    const VERSION = '1.2.0';
+    const VERSION = '1.2.1';
     
     public function registerBundles()
     {
