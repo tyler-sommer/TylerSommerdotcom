74ec36a6adc01f6f2c39c888fc046cb4cff39f8c
Tyler Sommer <sommertm@gmail.com>
2014-10-13 18:01:33 -0700

    Modified default date formatting

--- src/TylerSommer/Bundle/BlogBundle/TylerSommerBlogBundle.php
+++ src/TylerSommer/Bundle/BlogBundle/TylerSommerBlogBundle.php
@@ -9,8 +9,15 @@
 
 namespace TylerSommer\Bundle\BlogBundle;
 
+use Orkestra\Common\Type\Date;
+use Orkestra\Common\Type\DateTime;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TylerSommerBlogBundle extends Bundle
 {
+    public function boot()
+    {
+        DateTime::setDefaultFormat('m/d/Y');
+        Date::setDefaultFormat('m/d/Y');
+    }
 }
