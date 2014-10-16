c13d54678245a73cb6ce3a53c9283fcc85db87e0
Tyler Sommer <sommertm@gmail.com>
2014-10-15 21:51:13 -0700

    Tweaked syntax highlighting

--- src/TylerSommer/Bundle/BlogBundle/Resources/public/js/highlighter/shBrushGo.js
+++ src/TylerSommer/Bundle/BlogBundle/Resources/public/js/highlighter/shBrushGo.js
@@ -7,12 +7,12 @@ SyntaxHighlighter.brushes.Go = function()
 	var control_words = 'goto break continue if else switch default case for range go select return fallthrough defer';
 
 	this.regexList = [
-		{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'comments' },			// one line comments
-		{ regex: SyntaxHighlighter.regexLib.multiLineCComments,		css: 'comments' },			// multiline comments
-		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },			// strings
-		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },			// strings
-		{ regex: new RegExp(this.getKeywords(declaration_type_words), 'gm'),		css: 'color2 bold' },
-		{ regex: new RegExp(this.getKeywords(control_words), 'gm'),		css: 'keyword bold' }
+		{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	           css: 'comments' },
+		{ regex: SyntaxHighlighter.regexLib.multiLineCComments,		           css: 'comments' },
+		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		           css: 'string' },
+		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		           css: 'string' },
+		{ regex: new RegExp(this.getKeywords(declaration_type_words), 'gm'), css: 'keyword' },
+		{ regex: new RegExp(this.getKeywords(control_words), 'gm'),		       css: 'keyword' }
 		];
 };
 
